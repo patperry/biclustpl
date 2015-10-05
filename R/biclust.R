b598d6c919fda9a25f2e7bee4d155bf9f38102c4
Patrick Perry <patperry@gmail.com>
2015-10-04 20:05:50 -0400

    add support for multiple restarts

--- R/biclust.R
+++ R/biclust.R
@@ -43,22 +43,48 @@ validate_clusters <- function(n, clusters)
 
 
 biclust_dense <- function(x, row_clusters, col_clusters,
-                          family=c("gaussian", "poisson", "binomial"),
-                          epsilon=1e-6, maxit=10000L, trace=TRUE)
+                          family = c("gaussian", "poisson", "binomial"),
+                          nstart = 1,
+                          epsilon = 1e-6, maxit = 10000L, trace = FALSE)
 {
     x <- as.matrix(x)
     storage.mode(x) <- "double"
+    m <- nrow(x)
+    n <- ncol(x)
 
     row <- validate_clusters(nrow(x), row_clusters)
     col <- validate_clusters(ncol(x), col_clusters)
 
     family <- match.arg(family)
+
+    nstart <- round(nstart)
+    if (nstart < 1)
+        stop("invalid 'nstart' argument")
+
     storage.mode(epsilon) <- "double"
     storage.mode(maxit) <- "integer"
 
-    ans <- .Call(C_biclust_dense, x, row$nclusters, row$clusters - 1L,
-                 col$nclusters, col$clusters - 1L, family, epsilon, maxit,
-                 trace)
+    K <- row$nclusters
+    L <- col$nclusters
+    row_cl0 <- row$clusters - 1L
+    col_cl0 <- col$clusters - 1L
+
+    best <- NULL
+
+    for (start in seq_len(nstart)) {
+        ans <- .Call(C_biclust_dense, x, K, row_cl0, L, col_cl0,
+                     family, epsilon, maxit, trace)
+        if (start == 1 || ans$loglik > best$loglik) {
+            best <- ans
+        }
+        if (start < nstart) {
+            row_cl0 <- sample.int(K, m, replace=TRUE) - 1L
+            col_cl0 <- sample.int(L, n, replace=TRUE) - 1L
+        }
+    }
+
+    ans <- best
+
     ans$row_clusters <- ans$row_clusters + 1L
     ans$col_clusters <- ans$col_clusters + 1L
     means <- matrix(0, row$nclusters, col$nclusters)
